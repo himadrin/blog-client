4dd7ff20928cc7eefe14b8df473ada5c2efc2c23
himadri narasimhamurthy <himadri99@gmail.com>
2020-07-15 14:29:02 -0500

    responsive post styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import {PostMasonry} from '../components/common'
+import {PostMasonry, MasonryPost} from '../components/common'
 import featured from '../assets/mocks/featured'
 import trending from '../assets/mocks/trending'
 
 const trendingConfig = {
     1: {
         gridArea: '1 / 2 / 3 / 3'
+    }
+}
+
+const featuredConfig = {
+    0: {
+        gridArea: '1 / 1 / 2 / 3',
+        height: '300px'
+    },
+    1: {
+        height: '300px'
+    }, 
+    3: {
+        height: '630px',
+        marginLeft: '30px',
+        width: '630px'
     }
 }
 
@@ -16,12 +31,20 @@ const mergeStyles = function (posts, config) {
 }
 
 mergeStyles(trending, trendingConfig);
+mergeStyles(featured, featuredConfig);
+
+const lastFeat = featured.pop()
 
 export default function Home () {
     return (
         <section className="container home">
             <div className='row'>
-                <h2>trending</h2>
+                <h1>featured</h1>
+                <section className='featured-posts-container'>
+                    <PostMasonry posts={featured} columns={2} tagsOnTop={true}/>
+                    <MasonryPost post={lastFeat} tagsOnTop={true}></MasonryPost>
+                </section>
+                <h1>trending</h1>
                 <PostMasonry posts={trending} columns={3}/>
             </div>
         </section>
